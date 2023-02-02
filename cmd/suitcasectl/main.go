457789c1d025e6060dd5ad5a27a099092426e1ea
Drew Stinnett <drew.stinnett@duke.edu>
2023-02-02 14:33:59 -0500

    More wording for future-drew

--- cmd/suitcasectl/main.go
+++ cmd/suitcasectl/main.go
@@ -9,6 +9,11 @@ import (
 )
 
 func main() {
+	// We are pushing all the usage to Stdout instead of Stderr. I would
+	// like to eventually get this back to stderr, however currently that
+	// breaks the shell completion pieces, as all shells expect them on
+	// stdout. Hopefully cobra will be able to have multiple outputs at some
+	// point
 	err := cmd.NewRootCmd(os.Stdout).Execute()
 	if err != nil {
 		log.Fatal().Err(err).Msg("Error executing command")
