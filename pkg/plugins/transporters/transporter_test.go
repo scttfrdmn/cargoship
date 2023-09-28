e9c9733f158bb36d60965d870306be95e3be77ce
Drew Stinnett <drew.stinnett@duke.edu>
2023-09-28 09:24:54 -0400

    Adding tests

--- pkg/plugins/transporters/transporter_test.go
+++ pkg/plugins/transporters/transporter_test.go
@@ -1,6 +1,7 @@
 package transporters
 
 import (
+	"os"
 	"strings"
 	"testing"
 
@@ -11,4 +12,12 @@ func TestUniqifyDest(t *testing.T) {
 	got := UniquifyDest("foo:/bar")
 	require.True(t, strings.HasPrefix(got, "foo:/bar/"))
 	require.Greater(t, got, "foo:/bar/")
+}
+
+func TestToEnv(t *testing.T) {
+	c := Config{
+		Destination: "/tmp/foo",
+	}
+	require.NoError(t, c.ToEnv())
+	require.Equal(t, "/tmp/foo", os.Getenv("SUITCASECTL_DESTINATION"))
 }
